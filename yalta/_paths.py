c23939da92b54fe8ad39b7ac7fe716fb9c82db16
ryan-rashidian <ryanrashidian@gmail.com>
2025-12-03 15:18:44 -0800

    Update _paths.py module to declare constants for Path strings, looks nicer

--- yalta/_paths.py
+++ yalta/_paths.py
@@ -2,6 +2,9 @@
 
 from pathlib import Path
 
+APP_NAME = 'yalta'
+DATA_FILE = 'tasks.json'
+
 
 def ensure_file_exists(file_path: Path) -> None:
     """Checks if the data file exists. If not, it creates one."""
@@ -10,6 +13,6 @@ def ensure_file_exists(file_path: Path) -> None:
         file_path.write_text('{}', encoding='UTF-8')
 
 
-TASK_PATH = Path.home() / '.local' / 'share' / 'yalta' / 'tasks.json'
+TASK_PATH = Path.home() / '.local' / 'share' / APP_NAME / DATA_FILE
 ensure_file_exists(TASK_PATH)
 
